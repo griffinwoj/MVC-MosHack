0540fd73fe5902e0a5b7d802df9271bb6b00e690
griffinw@protonmail.com <griffinw@protonmail.com>
2023-05-16 20:28:47 -0400

    still struggling to run complete functionality

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,21 @@
 const Sequelize = require('sequelize');
 require('dotenv').config(); // Import sensitive data from .env
 
-const sequelize = process.env.JAWSDB_URL 
-  ? new Sequelize(process.env.JAWSDB_URL) // JAWSDB_URL 
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // for Local
-      // host: 'localhost',
-      // dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+let sequelize;
 
-module.exports = sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    "bucket_db",
+    "root",
+    "password",
+
+
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // Importing required modules and dependencies
-const express = require("express");
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
-const exphbs = require("express-handlebars");
-const hbs = exphbs.create({ helpers: require("./utils/helpers") });
+const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers: require('./utils/helpers') });
 // Creating express app and setting port
 
 const app = express();
